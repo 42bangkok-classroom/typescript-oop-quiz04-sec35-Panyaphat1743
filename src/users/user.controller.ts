d122bdf8e95b211398e2f113f8d14357ead3a686
panyaphat insawng <68011743@kmitl.ac.th>
2026-03-02 11:12:31 +0700

    testp02.1

--- src/users/user.controller.ts
+++ src/users/user.controller.ts
@@ -1,15 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { UserService } from './user.service';
 
-
-
 @Controller('users')
 export class UserController {
-    constructor(private readonly userservice: UserService){}
+  constructor(private readonly userservice: UserService) {}
 
-    @Get('test')
-    test(){
-        return this.userservice.test();
-    }
-
+  @Get('test')
+  test(): string[] {
+    return this.userservice.test();
+  }
 }
