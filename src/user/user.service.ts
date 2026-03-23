cbeb0c3c446bd43039e3541b315f08d562899b86
panyaphat insawng <68011743@kmitl.ac.th>
2026-03-24 00:07:23 +0700

    prep03.4

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { IUser } from './user.interface';
-import * as usersdata from '../../data/users.json';
+import usersdata from '../../data/users.json';
 
 @Injectable()
 export class UserService {
