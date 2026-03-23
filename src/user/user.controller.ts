65db1f2faffbc6ca5fd700cf2271f3b8d3fd6cac
panyaphat insawng <68011743@kmitl.ac.th>
2026-03-23 23:36:11 +0700

    prep03

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,12 +1,18 @@
 import { Controller, Get } from '@nestjs/common';
 import { UserService } from './user.service';
+import { IUser } from './user.interface'
 
-@Controller('users')
+@Controller()
 export class UserController {
   constructor(private readonly userservice: UserService) {}
 
-  @Get('test')
+  @Get('users/test')
   test(): [] {
     return this.userservice.test();
   }
+
+   @Get('users')
+   getAll(): IUser[]{
+    return this.userservice.findAll()
+   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@nestjs/common';
+import { IUser } from './user.interface'
+import * as usersdata from '../../data/users.json'
 
 @Injectable()
 export class UserService {
   test(): [] {
     return [];
+  }
+
+  findAll():IUser[]{
+    return usersdata
   }
 }
